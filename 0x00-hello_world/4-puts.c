951c40a0e58b7ddccd42f478fc8c46c130ba63dd
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-10 10:38:43 -0800

    C is live

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - function is the entry point that returns int
+*Return: return 0 after being called as a success
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
