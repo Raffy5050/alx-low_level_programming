bb87a74e1e6cfabe1a86c774c8a375b72b1999e3
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-10 11:33:10 -0800

    6size

---
+++ 0x00-hello_world/6-sizes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main - function is the entry point that returns int
+*Return: returns zero to whomever called it as a success
+*/
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+
