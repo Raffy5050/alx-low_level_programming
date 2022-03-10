83db9d12803af6023b18b96eef1586d022abe1a3
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-10 10:47:03 -0800

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - function is the entry point that returns integer value
+*Return: return 0 to whomever called it as a success
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
