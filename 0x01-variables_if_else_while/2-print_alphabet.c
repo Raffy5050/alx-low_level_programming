018074140ec9472895ca574e1de508bb45f5eb6e
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-11 09:08:53 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point of program
+*Return: return 0
+*/
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}
