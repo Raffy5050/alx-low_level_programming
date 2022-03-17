5e4f1fdab28acb2cd7cb50e6db1e871752a6ded2
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-17 14:45:46 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,26 +7,14 @@ include "main.h"
 *Return: 0 as success
 */
 
-
-
 void print_most_numbers(void)
-
 {
-
-	char i;
-
-
-
-	for (i = '0'; i <= '9'; i++)
-
-	{
-
-		if (i != '2' && i != '4')
-
-			_putchar(i);
-
-	}
-
-	_putchar('\n');
-
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '2' && i != '4')
+_putchar(i);
 }
+_putchar('\n');
+}
+
