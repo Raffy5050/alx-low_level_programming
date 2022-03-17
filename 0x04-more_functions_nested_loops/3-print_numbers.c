251e4d0353ad2a8bcc3b0aa7aa2ad4fe3c49a5ae
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-17 14:30:25 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_numbers - print numbers form 0 to 9
+*/
+
+
+
+void print_numbers(void)
+
+{
+
+	char i;
+
+
+
+	for (i = '0'; i <= '9'; i++)
+
+		_putchar(i);
+
+
+
+	_putchar('\n');
+
+}
