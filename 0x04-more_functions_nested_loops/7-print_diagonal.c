e6223cc007f0c09bf6bc7806f52aa3a583bdcc24
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-17 15:12:23 -0700

    diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+*print_diagonal - print character "\\" n times
+*@n: parameter needed
+*Return: 0 as success
+*/
+
+
+
+void print_diagonal(int n)
+
+{
+
+	int draw;
+
+	int space;
+
+
+
+	if (n > 0)
+
+	{
+
+		for (draw = 1; draw <= n; draw++)
+
+		{
+
+			for (space = 1; space < draw; space++)
+
+				_putchar(' ');
+
+			_putchar('\\');
+
+			_putchar('\n');
+
+		}
+
+	}
+
+	else
+
+	{
+
+		_putchar('\n');
+
+	}
+
+}
+
