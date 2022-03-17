79acc5c61cb47f8107db225462adb984b2803c24
Jamiu Rofiat <yungsophie21@gmail.com>
2022-03-17 14:21:17 -0700

    multiply two values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+
+/**
+*mul - multiply two integers
+*@a: first parameter
+*@b: second parameter
+*Return: 0 as success
+*/
+
+
+
+int mul(int a, int b)
+
+{
+
+	return (a * b);
+
+}
+
