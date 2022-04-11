64cdabb0abcc95a9cae400303831744569393721
Jamiu Rofiat <yungsophie21@gmail.com>
2022-04-11 13:16:03 -0700

    update

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MSG "Hello, Holberton"
+
+
+
+/**
+*main - prints a message.
+*Return: Always 0.
+*/
+
+int main(void)
+
+{
+
+	printf("%s\n", MSG);
+
+	return (0);
+
+}
